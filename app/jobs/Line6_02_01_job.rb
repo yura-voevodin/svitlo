b3c947d77282c89f139bb8e93336c06d1bb3aa41
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-02 07:48:38 +0200

    Add Job for Line 6

---
+++ app/jobs/Line6_02_01_job.rb
@@ -0,0 +1,36 @@
+class Line60201Job < ApplicationJob
+  queue_as :default
+  
+  def perform(*args)
+    day = Day.where(line_id: 6, date: Date.today).first
+    if day.nil?
+      day = Day.new(date: Date.today, line_id: 6)
+    end
+    day.time1 = "on"
+    day.time2 = "on"
+    day.time3 = "on"
+    day.time4 = "off"
+    day.time5 = "maybe"
+    day.time6 = "on"
+    day.time7 = "on"
+    day.time8 = "on"
+    day.time9 = "on"
+    day.time10 = "off"
+    day.time11 = "off"
+    day.time12 = "on"
+    day.time13 = "on"
+    day.time14 = "on"
+    day.time15 = "on"
+    day.time16 = "off"
+    day.time17 = "off"
+    day.time18 = "maybe"
+    day.time19 = "on"
+    day.time20 = "on"
+    day.time21 = "on"
+    day.time22 = "off"
+    day.time23 = "off"
+    day.time24 = "off"
+    day.save
+  end
+  
+end
