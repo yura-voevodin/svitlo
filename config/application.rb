ef9a8a2edd279c98caafc060823627431d8893d5
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-02 09:32:25 +0200

    Add Ukrainian locale

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,9 @@ module Svitlo
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    config.i18n.default_locale = :uk
+    I18n.available_locales = [:en, :uk]
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
