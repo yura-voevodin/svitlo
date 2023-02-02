21f785c947b347fdf1d8a61f1e495b52f1efa297
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-02 10:01:17 +0200

    Refactoring

--- app/controllers/lines_controller.rb
+++ app/controllers/lines_controller.rb
@@ -2,5 +2,6 @@ class LinesController < ApplicationController
   def show
     @line = Line.find(params[:id])
     @day = Day.where(line: @line, date: Date.today).first
+    @date = @day.present? ? @day.date : Date.today
   end
 end
