f97c7e0b3b7a133023179c845d7a2d130ee89acf
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-03 11:31:06 +0200

    Fix today date

--- app/jobs/lines_job.rb
+++ app/jobs/lines_job.rb
@@ -2,7 +2,7 @@ class LinesJob < ApplicationJob
   queue_as :default
   
   def perform(*args)
-    date = '03.01.2023'.to_date
+    date = '03.02.2023'.to_date
     Line1Job.perform_now(date: date)
     Line2Job.perform_now(date: date)
     Line3Job.perform_now(date: date)
