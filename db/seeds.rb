b867b70512c365167c90bf5a8dc0764adec503ed
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-05 21:38:11 +0200

    Update lines data

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,13 +6,29 @@
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
 
-Line.create([
-    { name: "Підчерги 1.1, 1.2" },
-    { name: "Підчерги 1.3, 1.4" },
-    { name: "Підчерги 2.1, 2.2" },
-    { name: "Підчерги 2.3, 2.4" },
-    { name: "Підчерги 3.1, 3.2" },
-    { name: "Підчерги 3.3, 3.4" },
-    { name: "Підчерга 4.1" },
-    { name: "Підчерга 4.2" }
-])
+lines = [
+    { id: 1, name: "Підчерга 1.1" },
+    { id: 2, name: "Підчерга 1.2" },
+    { id: 3, name: "Підчерга 1.3" },
+    { id: 4, name: "Підчерга 1.4" },
+    { id: 5, name: "Підчерга 2.1" },
+    { id: 6, name: "Підчерга 2.2" },
+    { id: 7, name: "Підчерга 2.3" },
+    { id: 8, name: "Підчерга 2.4" },
+    { id: 9, name: "Підчерга 3.1" },
+    { id: 10, name: "Підчерга 3.2" },
+    { id: 11, name: "Підчерга 3.3" },
+    { id: 12, name: "Підчерга 3.4" },
+    { id: 13, name: "Підчерга 4.1" },
+    { id: 14, name: "Підчерга 4.2" }
+]
+
+lines.each do |line_data|
+    line = Line.find_by(id: line_data[:id])
+    if line.nil?
+        line = Line.new
+        line.id = line_data[:id]
+        line.name = line_data[:name]
+        line.save
+    end
+end
