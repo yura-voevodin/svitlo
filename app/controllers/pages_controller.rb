0e52f2cf02648ead9ac4e5ada2ec8f12726035f7
Yura Voevodin <voevodin.yura@icloud.com>
2023-02-08 20:59:56 +0200

    Add privacy policy and terms of service

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,13 @@
 class PagesController < ApplicationController
-    def contacts
-        render "pages/contacts"
-    end
+  def contacts
+    render "pages/contacts"
+  end
+
+  def privacy_policy
+    render "pages/privacy-policy"
+  end
+
+  def terms_of_service
+    render "pages/terms-of-service"
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -6,6 +6,8 @@ Rails.application.routes.draw do
 
   # Pages
   get "/contacts", to: "pages#contacts"
+  get "/privacy-policy", to: "pages#privacy_policy"
+  get "/terms-of-service", to: "pages#terms_of_service"
 
   resources :lines, only: [:show], param: :id do
     member do
